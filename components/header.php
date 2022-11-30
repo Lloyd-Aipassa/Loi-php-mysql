b5dd36fc6636698aef563bffaab586cb8044e382
Lloyd Aipassa <lloyd.aipassa@gmail.com>
2022-11-30 23:49:58 +0100

    header aangepast

--- components/header.php
+++ components/header.php
@@ -1,22 +1,23 @@
 <style>
-header {
-	width: 100%;
-	justify-content: space-between;
-	display: flex;
-	height: 10vh;
-	align-items: center;
-	padding: 0 50px;
-    background-color: #2196f3;
-}
+	header {
+		width: 100%;
+		justify-content: space-between;
+		display: flex;
+		height: 10vh;
+		align-items: center;
+		padding: 0 50px;
+		background-color: #2196f3;
+	}
 
-header img {
-	height: 100%;
-	padding: 5px 0;
-}</style>
+	header img {
+		height: 100%;
+		padding: 5px 0;
+	}
+</style>
 
 <body>
 
-<header>
-    <h1>logo 33</h1>
-    <img src="https://qwicko.nl/wp-content/uploads/2022/11/ltvBreukelen.png" alt="logo">
-</header>
+	<header>
+		<h1>logo 33</h1>
+		<img src="pngwing.com.png" alt="logo">
+	</header>
