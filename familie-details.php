c8938c095115aa0d84c545e5eb5e17b78b4b87c8
Lloyd Aipassa <lloyd.aipassa@gmail.com>
2022-12-03 00:03:54 +0100

    delete functie toegevoegd

--- familie-details.php
+++ familie-details.php
@@ -3,6 +3,21 @@ $pageTitle = 'Familie-details';
 
 include('config/db.php');
 
+if (isset($_POST['delete'])) {
+    $id_delete = mysqli_real_escape_string($conn, $_POST['id_delete']);
+
+    $sql = "DELETE FROM families WHERE id = $id_delete";
+
+    if(mysqli_query($conn, $sql)){
+        //succes yes!!
+        header('location: index.php');
+        {
+            //geen yes
+            echo 'query error:' . mysqli_error($conn);
+        }
+    }
+} 
+
 //  check GET request id param -- (in de url)
 if (isset($_GET['id'])) {
 
@@ -21,7 +36,7 @@ if (isset($_GET['id'])) {
     mysqli_free_result($result);
     mysqli_close($conn);
 
-    print_r($familie);
+    // print_r($familie);
 }
 ?>
 
@@ -45,7 +60,23 @@ if (isset($_GET['id'])) {
         <?php include('components/nav.php') ?>
     </aside>
     <main class="main">
-        test
+        <div class="card-details">
+            <?php if ($familie) : ?>
+                <h1>Familie details</h1>
+                <h3>Achternaaam: <?php echo htmlspecialchars($familie['achternaam']); ?></h3>
+                <h3>Adres: <?php echo htmlspecialchars($familie['adres']); ?></h3>
+
+                <!-- delete familie dmv een form met hidden input -->
+
+                <form action="familie-details.php" method="POST">
+                    <input type="hidden" name="id_delete" value="<?php  echo $familie['id'] ?>">
+                    <input type="submit" name="delete" value="delete">
+                </form>
+                <button>delete familie</button>
+            <?php else :  ?>
+                <h2>Familie bestaat niet!</h2>
+            <?php endif; ?>
+        </div>
     </main>
 </div>
 
@@ -53,3 +84,39 @@ if (isset($_GET['id'])) {
 
 
 </html>
+
+<style>
+    .card-details {
+        width: 100%;
+        max-width: 500px;
+        height: 500px;
+        border: 1px solid #fff;
+        margin: auto;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        /* justify-content: center; */
+        border-radius: 8px;
+        background-color: #1d1d1d;
+        position: relative;
+        padding-top: 200px;
+    }
+
+    .card-details h1 {
+        position: absolute;
+        top: 30px;
+        left: 50%;
+        transform: translateX(-50%);
+        margin: 0 0 20px 0;
+        color: coral;
+    }
+
+    .card-details h3 {
+        margin: 0px 0 10px 0;
+    }
+
+    main .card-details button {
+        margin-top: 50px;
+        background-color: coral;
+    }
+</style>
