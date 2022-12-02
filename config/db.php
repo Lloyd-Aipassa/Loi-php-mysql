23346b72f09f57c312c6bd3827aa9958e9abafc9
Lloyd Aipassa <lloyd.aipassa@gmail.com>
2022-12-02 22:56:57 +0100

    swap connection

--- config/db.php
+++ config/db.php
@@ -1,8 +1,12 @@
 <?php
 
-$conn = mysqli_connect('loi.brooklynwebdesign.nl', 'lloyd', 'L249m66a', 'lloyd');
+$conn = mysqli_connect('localhost', 'lloyd', 'lloyd123', 'lloyd');
+
+// $conn = mysqli_connect('loi.brooklynwebdesign.nl', 'lloyd', 'L249m66a', 'lloyd');
 
 // check connection
 if (!$conn) {
     echo 'Connetion error: ' . mysqli_connect_error();
 }
+
+?>
